e00e3f6edabeef5e97f64bedc7ede2960c7531ec
Muqeet Ahmed <solangimuqueet@gmail.com>
2024-07-29 04:32:54 -0700

    Make Sliders and Banner Hero and Pumflate

--- Assets/Javascript/Slider.js
+++ Assets/Javascript/Slider.js
@@ -1,37 +1,36 @@
 var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 30,
-    effect: "fade",
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    loop: true,
+  spaceBetween: 30,
+  effect: "fade",
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  autoplay: true,
+  loop : true
 });
 
-// Cards-Section-Slider ====================
-var swiper = new Swiper(".MySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 10,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+
+var swiper = new Swiper(".MYSwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop : true,
+  autoplay: true,
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
     },
-    breakpoints: {
-      640: {
-        slidesPerView: 2,
-        spaceBetween: 20,
-      },
-      768: {
-        slidesPerView: 4,
-        spaceBetween: 40,
-      },
-      1024: {
-        slidesPerView: 5,
-        spaceBetween: 50,
-      },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
     },
-  });
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 5,
+    },
+  },
+});
